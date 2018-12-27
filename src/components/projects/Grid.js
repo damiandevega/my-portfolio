dbdfdb30d788ae634aeb9e87c65e5d24474a20b1
Damian de Vega <damiandevega@gmail.com>
2018-12-27 15:51:59 -0500

    adding extra content to projects section

--- src/components/projects/Grid.js
+++ src/components/projects/Grid.js
@@ -36,7 +36,7 @@ function CenteredGrid(props) {
     <div className={classes.root}>
       <Grid container spacing={24}>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
+        <Grid item xs={12} sm={12} md={4}>
             <Bounce left delay={0} duration={1300}>
                 <Media 
                     imagePath={bloom_kauai} 
@@ -44,42 +44,21 @@ function CenteredGrid(props) {
                     description="Floral business in Kauai for leis, flower crowns, bouquets, arrangements, ceremonies etc."
                     link="http://kauai-flowers.s3-website-us-west-1.amazonaws.com/"
                 />
-                {/* <Paper className={classes.paper}>
-                    <div className="hovereffect">
-                        <img style={{ position: "relative", width: "100%", height: "100%" }} className="hovereffect" src={bloom_kauai} alt="bloom kauai"></img>
-                        <div className="overlay">
-                            <h2>bloom kauai</h2>
-                            <p style={{color: "lightblue"}}><strong>project description</strong></p>
-                            <a className="info" href="http://kauai-flowers.s3-website-us-west-1.amazonaws.com/index.html" target="_blank" rel="noopener noreferrer">go to site</a>
-                        </div>
-                    </div>
-                </Paper> */}
             </Bounce>
         </Grid>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-            <Bounce right delay={0} duration={1300}>
+        <Grid item xs={12} sm={12} md={4}>
+            <Bounce left delay={0} duration={1300}>
                 <Media 
                     imagePath={developer_connector} 
                     title="developer connector" 
                     description="A social network for developers to connect and ask questions"
                     link="http://www.developerconnector.com/"
                 />
-                {/* <Paper className={classes.paper}>
-                    <div className="hovereffect">
-                        <img className="card" src={developer_connector} alt="developer connector"></img>
-                        <div className="overlay">
-                            <h2>developer connector</h2>
-                            <p style={{color: "lightblue"}}><strong>project description</strong></p>
-                            <a className="info" href="http://github-finder.s3-website-us-west-2.amazonaws.com/" target="_blank" rel="noopener noreferrer">go to site</a>
-                        </div>
-                    </div>
-                </Paper> */}
             </Bounce>
         </Grid>
 
-
-        <Grid item xs={12} sm={12} md={6} lg={6}>
+        <Grid item xs={12} sm={12} md={4}>
             <Bounce left delay={0} duration={1300}>
                 <Media 
                     imagePath={animal_hospital} 
@@ -87,20 +66,21 @@ function CenteredGrid(props) {
                     description="Animal hospital website focused on veterinary services"
                     link="http://animal-hospital-website-dev.s3-website-us-east-1.amazonaws.com/index.html"
                 />
-                {/* <Paper className={classes.paper}>
-                    <div className="hovereffect">
-                        <img style={{ width: "100%", height: "100%" }} className="hovereffect" src={animal_hospital} alt="animal hospital"></img>
-                        <div className="overlay">
-                            <h2>Animal Hospital</h2>
-                            <p style={{color: "lightblue"}}><strong>project description</strong></p>
-                            <a className="info" href="http://animal-hospital-website-dev.s3-website-us-east-1.amazonaws.com/index.html" target="_blank" rel="noopener noreferrer">go to site</a>
-                        </div>
-                    </div>
-                </Paper> */}
             </Bounce>
         </Grid>
 
-        <Grid item xs={12} sm={12} md={6} lg={6}>
+        <Grid item xs={12} sm={12} md={4}>
+            <Bounce right delay={0} duration={1300}>
+                <Media 
+                    imagePath={animal_hospital} 
+                    title="animal hospital" 
+                    description="Animal hospital website focused on veterinary services"
+                    link="http://animal-hospital-website-dev.s3-website-us-east-1.amazonaws.com/index.html"
+                />
+            </Bounce>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={4}>
             <Bounce right delay={0} duration={1300}>
                 <Media 
                     imagePath={movie_info} 
@@ -108,16 +88,17 @@ function CenteredGrid(props) {
                     description="A fun search tool to find your favorite movies or shows from their titles"
                     link="http://movie-info.s3-website-us-west-2.amazonaws.com/index.html"
                 />
-                {/* <Paper className={classes.paper}>
-                    <div className="hovereffect">
-                        <img style={{ width: "100%", height: "100%" }} className="hovereffect" src={movie_info} alt="movie info"></img>
-                        <div className="overlay">
-                            <h2>Movie Info</h2>
-                            <p style={{color: "lightblue"}}><strong>project description</strong></p>
-                            <a className="info" href="http://movie-info.s3-website-us-west-2.amazonaws.com/index.html" target="_blank" rel="noopener noreferrer">go to site</a>
-                        </div>
-                    </div>
-                </Paper> */}
+            </Bounce>
+        </Grid>
+
+        <Grid item xs={12} sm={12} md={4}>
+            <Bounce right delay={0} duration={1300}>
+                <Media 
+                    imagePath={movie_info} 
+                    title="movie info" 
+                    description="A fun search tool to find your favorite movies or shows from their titles"
+                    link="http://movie-info.s3-website-us-west-2.amazonaws.com/index.html"
+                />
             </Bounce>
         </Grid>
 
