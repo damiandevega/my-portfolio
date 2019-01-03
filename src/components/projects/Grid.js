8b04a27a91a932acc167154ccd96604649567e17
Damian de Vega <damiandevega@gmail.com>
2019-01-03 18:45:29 -0500

    added new image for furniture store entry to projects section

--- src/components/projects/Grid.js
+++ src/components/projects/Grid.js
@@ -10,9 +10,10 @@ import Media from './Media';
 import developer_connector from "./images/developer-connector.png";
 import bloom_kauai from "./images/bloom-kauai.png";
 import animal_hospital from "./images/animal-hospital.png";
-import movie_info from "./images/movie-info.png";
+// import movie_info from "./images/movie-info.png";
 import grand_hotel from "./images/grand-hotel.png";
 import data_visualization from "./images/data-visualization.png";
+import furniture_store from "./images/furniture-store.png";
 
 const styles = theme => ({
   root: {
@@ -96,7 +97,7 @@ function CenteredGrid(props) {
         <Grid item xs={12} sm={6} md={4}>
             <Bounce right delay={500} duration={1300}>
                 <Media 
-                    imagePath={movie_info} 
+                    imagePath={furniture_store} 
                     title="movie info" 
                     description="A fun search tool to find your favorite movies or shows from their titles"
                     link="http://movie-info.s3-website-us-west-2.amazonaws.com/index.html"
