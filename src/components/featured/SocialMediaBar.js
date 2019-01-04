1cb27f86816855d859654a9a9bf434208bc47582
Damian de Vega <damiandevega@gmail.com>
2019-01-03 19:07:27 -0500

    added target = _blank to social media links

--- src/components/featured/SocialMediaBar.js
+++ src/components/featured/SocialMediaBar.js
@@ -5,8 +5,8 @@ class SocialMediaBar extends Component {
         return (
             <div className="countdown_wrapper">
                 <div className="icon-bar">
-                    <a href="https://github.com/damiandevega" className="github"><i className="fa fa-github"></i></a> 
-                    <a href="https://www.linkedin.com/in/damian-de-vega-64333a66/" className="linkedin"><i className="fa fa-linkedin"></i></a>
+                    <a href="https://github.com/damiandevega" className="github" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"></i></a> 
+                    <a href="https://www.linkedin.com/in/damian-de-vega-64333a66/" className="linkedin" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"></i></a>
                 </div>
             </div>
         )
