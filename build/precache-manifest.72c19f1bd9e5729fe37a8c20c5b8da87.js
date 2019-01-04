0b0f9cb39d75dbb23a16e6edc8f9b9acae0fa968
Damian de Vega <damiandevega@gmail.com>
2019-01-04 09:56:41 -0500

    adding build files to repo

---
+++ build/precache-manifest.72c19f1bd9e5729fe37a8c20c5b8da87.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "4befb1a465da84136847eee92cd8701e",
+    "url": "/static/media/bloom-kauai.4befb1a4.png"
+  },
+  {
+    "revision": "2484b749415f5bfa3780",
+    "url": "/static/css/main.16da8397.chunk.css"
+  },
+  {
+    "revision": "b83940a8e0a5b12dd2ec",
+    "url": "/static/js/1.b83940a8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61d041effdda6c2a54c6501ae1984f57",
+    "url": "/static/media/creative.61d041ef.png"
+  },
+  {
+    "revision": "878e153ee1c52998ed178903b1969685",
+    "url": "/static/media/damiandevega.878e153e.png"
+  },
+  {
+    "revision": "fd07f52b3af976df99a6078aea0f9cdf",
+    "url": "/static/media/developer-connector.fd07f52b.png"
+  },
+  {
+    "revision": "2484b749415f5bfa3780",
+    "url": "/static/js/main.2484b749.chunk.js"
+  },
+  {
+    "revision": "630b29beee64993754949e8141327d7c",
+    "url": "/static/media/animal-hospital.630b29be.png"
+  },
+  {
+    "revision": "aa4cf60251ad7dec2fbafafbe92191de",
+    "url": "/static/media/grand-hotel.aa4cf602.png"
+  },
+  {
+    "revision": "72d3cb52ff79370bfdf33734faa7d0f0",
+    "url": "/static/media/data-visualization.72d3cb52.png"
+  },
+  {
+    "revision": "2c70413a50e4194f29473af01719380b",
+    "url": "/static/media/furniture-store.2c70413a.png"
+  },
+  {
+    "revision": "4553a5e734f762991d62a59996fc41a0",
+    "url": "/static/media/damian_de_vega_resume.4553a5e7.pdf"
+  },
+  {
+    "revision": "88095c8836bbe0be4ae9b863f407e704",
+    "url": "/static/media/damian-landing-2.88095c88.png"
+  },
+  {
+    "revision": "f0d44661596a8be5b82b70cde08a4281",
+    "url": "/index.html"
+  }
+];
